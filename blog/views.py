809afed3052659082d9424cf25f8cc3541411aaa
duongbm <mduong1995@gmail.com>
2022-10-06 09:58:04 +0700

    [feat] add similar post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Count
 # Create your views here.
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -43,7 +43,14 @@ def post_detail(request, year, month, day, post):
                              publish__month=month, publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'detail.html', {'post': post, 'comments': comments, 'form': form})
+
+    # list similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'detail.html',
+                  {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
